2675f0926d1f5d0043a7dbaf9c55f657d2861079
Ethan Hoffman <thatsnotgaca@gmail.com>
2026-02-26 18:33:02 -0800

    Node Update

--- atds.py
+++ atds.py
@@ -70,6 +70,23 @@ class Deque:
     def __str__(self):
         return "Deque[" + ",".join(self.items) + "]"
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
 if __name__ == "__main__":
     stack = Stack()
     print("Popping from empty stack:", stack.pop())
@@ -88,3 +105,12 @@ if __name__ == "__main__":
     print(dq.remove_front())
     print(dq.remove_rear())
     print(dq)
+    node1 = Node(100)
+    node2 = Node(200)
+    print("Node1 data: " + str(node1.get_data()))
+    print("Node1 next: " + str(node1.get_next()))
+    node1.set_next(node2)
+    print("After linking node1 to node2:")
+    print("Node1 next data: " + str(node1.get_next().get_data()))
+    node1.set_data(150)
+    print("After changing node1 data: " + str(node1.get_data()))
